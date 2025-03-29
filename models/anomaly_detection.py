270929df219a07576dc947dec6802293715a9f9e
Suvajit Lodh <x23356987@student.ncirl.ie>
2025-03-29 09:57:02 +0000

    re intialized the changes

--- models/anomaly_detection.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-from tensorflow.keras.models import load_model
-
-model = tf.keras.models.load_model('lstm_autoencoder.keras')
-
-def detect(sequence, threshold):
-    reconstruction = model.predict(sequence.reshape(1, *sequence.shape))
-    error = np.mean((sequence - reconstruction)**2)
-    return error > threshold
